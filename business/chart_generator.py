e75606599fb5f39e7d158f9c20f6693fd4bd089c
ShvedIryna <151570555+ShvedIryna@users.noreply.github.com>
2026-03-15 13:10:52 +0200

    Update chart_generator.py

--- business/chart_generator.py
+++ business/chart_generator.py
@@ -1,28 +1,6 @@
-from data_access.models import Report
+class ChartGenerator:
+    def generateIncomeChart(self):
+        print("Income chart generated")
 
-
-class FinancialManager:
-
-    def __init__(self, repository, csv_reader):
-
-        self.repository = repository
-        self.csv_reader = csv_reader
-
-    def processData(self, file):
-
-        data = self.csv_reader.read(file)
-
-        total_income = sum(d.income for d in data)
-        total_expense = sum(d.expense for d in data)
-
-        report = Report(
-            period="2025",
-            totalIncome=total_income,
-            totalExpense=total_expense,
-            netProfit=total_income - total_expense
-        )
-
-        self.repository.save_financial_data(data)
-        self.repository.save_report(report)
-
-        return report
+    def generateExpenseChart(self):
+        print("Expense chart generated")
